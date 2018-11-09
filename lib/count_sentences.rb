18fb86b814f482b13e307a258743d975440e742a
Paula McKinley <pmknyc@optimum.net>
2018-11-09 18:08:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,22 @@ require 'pry'
 
 class String
 
+# if string end in period ? true : false
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    self.split(/[!?.]+/).length
 
   end
 end
